7f3d3f08891716068d6a91605d98d6ab17546efc
Igor Okulist <okigan@gmail.com>
2016-10-12 09:25:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='awscurl',
-    version='0.7',
+    version='0.8',
     description='Curl like tool with AWS request signing',
     url='http://github.com/okigan/awscurl',
     author='Igor Okulist',
