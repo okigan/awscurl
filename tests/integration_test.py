eb90886eb306e37d5ca57a3d2dba763df23c6d76
Igor Okulist <iokulist@netflix.com>
2017-07-25 15:53:33 -0700

    Add integration test

---
+++ tests/integration_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import datetime
+import logging
+
+from unittest import TestCase
+
+from awscurl.awscurl import make_request
+
+__author__ = 'iokulist'
+
+
+class TestMakeRequestWithToken(TestCase):
+    maxDiff = None
+
+    def test_make_request(self, *args, **kvargs):
+        headers = {}
+        params = {'method': 'GET',
+                  'service': 's3',
+                  'region': 'us-east-1',
+                  'uri': 'https://awscurl-sample-bucket.s3.amazonaws.com',
+                  'headers': headers,
+                  'data': '',
+                  'profile': '',
+                  'access_key': 'AKIAIKDYYMAAECYXKVZQ',
+                  'secret_key': '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',
+                  'security_token': None}
+
+        make_request(**params)
+
+
+        pass
