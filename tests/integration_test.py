dab0ec6a0744f665a9a8101b23979cfa18edbdbb
Mario Sotil <mario.sotil@gmail.com>
2018-06-09 15:46:26 -0700

    Changed parameter from --binary-payload to --data-binary, just like curl

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -27,7 +27,29 @@ class TestMakeRequestWithToken(TestCase):
                   'access_key': access_key,
                   'secret_key': secret_key,
                   'security_token': None,
-                  'binary_payload': False}
+                  'data_binary': False}
+
+        r = make_request(**params)
+
+        self.assertEqual(r.status_code, 200)
+
+class TestMakeRequestWithTokenAndBinaryData(TestCase):
+    maxDiff = None
+
+    def test_make_request(self, *args, **kvargs):
+        headers = {}
+        access_key = base64.b64decode('QUtJQUkyNkxPQU5NSlpLNVNQWUE=').decode("utf-8")
+        secret_key = base64.b64decode('ekVQbE9URjU0Mys5M0l6UlNnNEVCOEd4cjFQV2NVa1p0TERWSmY4ag==').decode("utf-8")
+        params = {'method': 'GET',
+                  'service': 's3',
+                  'region': 'us-east-1',
+                  'uri': 'https://awscurl-sample-bucket.s3.amazonaws.com/awscurl-sample-file:.txt?a=b',
+                  'headers': headers,
+                  'data': 'C\xcfI\x91\xc1\xd0\tw<\xa8\x13\x06{=\x9b\xb3\x1c\xfcl\xfe\xb9\xb18zS\xf4%i*Q\xc9v',
+                  'access_key': access_key,
+                  'secret_key': secret_key,
+                  'security_token': None,
+                  'data_binary': True}
 
         r = make_request(**params)
 
