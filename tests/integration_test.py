4ad3c0ea98735116c4f6095de03088df900f9ce0
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2019-11-20 11:54:53 +0900

    Add integration test for English and non-English data

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -54,3 +54,47 @@ class TestMakeRequestWithTokenAndBinaryData(TestCase):
         r = make_request(**params)
 
         self.assertEqual(r.status_code, 200)
+
+class TestMakeRequestWithTokenAndEnglishData(TestCase):
+    maxDiff = None
+
+    def test_make_request(self, *args, **kvargs):
+        headers = {}
+        access_key = base64.b64decode('QUtJQUkyNkxPQU5NSlpLNVNQWUE=').decode("utf-8")
+        secret_key = base64.b64decode('ekVQbE9URjU0Mys5M0l6UlNnNEVCOEd4cjFQV2NVa1p0TERWSmY4ag==').decode("utf-8")
+        params = {'method': 'GET',
+                  'service': 's3',
+                  'region': 'us-east-1',
+                  'uri': 'https://awscurl-sample-bucket.s3.amazonaws.com/awscurl-sample-file:.txt?a=b',
+                  'headers': headers,
+                  'data': 'Test',
+                  'access_key': access_key,
+                  'secret_key': secret_key,
+                  'security_token': None,
+                  'data_binary': False}
+
+        r = make_request(**params)
+
+        self.assertEqual(r.status_code, 200)
+
+class TestMakeRequestWithTokenAndNonEnglishData(TestCase):
+    maxDiff = None
+
+    def test_make_request(self, *args, **kvargs):
+        headers = {}
+        access_key = base64.b64decode('QUtJQUkyNkxPQU5NSlpLNVNQWUE=').decode("utf-8")
+        secret_key = base64.b64decode('ekVQbE9URjU0Mys5M0l6UlNnNEVCOEd4cjFQV2NVa1p0TERWSmY4ag==').decode("utf-8")
+        params = {'method': 'GET',
+                  'service': 's3',
+                  'region': 'us-east-1',
+                  'uri': 'https://awscurl-sample-bucket.s3.amazonaws.com/awscurl-sample-file:.txt?a=b',
+                  'headers': headers,
+                  'data': 'テスト',
+                  'access_key': access_key,
+                  'secret_key': secret_key,
+                  'security_token': None,
+                  'data_binary': False}
+
+        r = make_request(**params)
+
+        self.assertEqual(r.status_code, 200)
