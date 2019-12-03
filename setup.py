7acfc3db667ad2c775f90343efaf6c6d6d52b797
Igor <okigan@gmail.com>
2019-12-03 00:49:45 -0800

    release version 0.18

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='awscurl',
-    version='0.17',
+    version='0.18',
     description='Curl like tool with AWS request signing',
     url='http://github.com/okigan/awscurl',
     author='Igor Okulist',
