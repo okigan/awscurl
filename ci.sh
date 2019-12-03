936578de483aafaf4f9a4da68f063afac1036117
Igor Okulist <iokulist@netflix.com>
2019-12-03 03:04:53 -0800

    add code coverage

--- ci.sh
+++ ci.sh
@@ -4,8 +4,9 @@ virtualenv venv -p python2.7
 
 source venv/bin/activate
 
-pip install requirements.txt
-pip install requirements-test.txt
+pip install -r requirements.txt
+pip install -r requirements-test.txt
 
 pycodestyle -v awscurl
-py.test -v
+
+pytest -v --cov=awscurl --cov-report html --cov-report annotate
