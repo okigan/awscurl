ee6e2b3bc12457edbdd906c677e39e255133109e
Igor Okulist <okigan@gmail.com>
2021-03-15 16:37:19 -0700

    Add ci-in-docker.sh to verify ubuntu and centos builds on py27,py36,py38

--- ci.sh
+++ ci.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-virtualenv venv -p python2.7
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
 
-source venv/bin/activate
-
-pip install -r requirements.txt
-pip install -r requirements-test.txt
-
-pycodestyle -v awscurl
-
-export AWS_ACCESS_KEY_ID=MOCK_AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=MOCK_AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN=MOCK_AWS_SESSION_TOKEN
-
-pytest -v --cov=awscurl --cov-fail-under=77 --cov-report html --cov-report annotate
+tox
