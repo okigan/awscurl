05b2fe23e57d933483cb382365c4974fe3b72940
Toshihiko Makita <tmakita@antenna.co.jp>
2021-04-24 02:56:28 +0900

    Update awscurl.py
    Treat --data-binary as true binary file

--- awscurl/awscurl.py
+++ awscurl/awscurl.py
@@ -132,10 +132,9 @@ def make_request(method,
     headers.update(auth_headers)
 
     if data_binary:
-        return __send_request(uri, data.encode('utf-8'), headers, method, verify)
-    else:
         return __send_request(uri, data, headers, method, verify)
-
+    else:
+        return __send_request(uri, data.encode('utf-8'), headers, method, verify)
 
 # pylint: disable=too-many-arguments,too-many-locals
 def task_1_create_a_canonical_request(
@@ -197,7 +196,7 @@ def task_1_create_a_canonical_request(
 
     # Step 6: Create payload hash (hash of the request body content). For GET
     # requests, the payload is an empty string ("").
-    payload_hash = sha256_hash(data)
+    payload_hash = sha256_hash_for_binary_data(data) if data_binary else sha256_hash(data)
 
     # Step 7: Combine elements to create create canonical request
     canonical_request = (method + '\n' +
@@ -446,17 +445,20 @@ def inner_main(argv):
 
     data = args.data
 
+    if args.header is None:
+        args.header = default_headers
+
     if data is not None and data.startswith("@"):
         filename = data[1:]
         if args.data_binary:
-            with open(filename, "r", encoding='UTF-8') as post_data_file:
+            with open(filename, "rb") as post_data_file:
+                data = post_data_file.read()
+        elif "Content-Type: application/json" in args.header:
+            with open(filename, "r", "UTF-8") as post_data_file:
                 data = post_data_file.read()
         else:
             with open(filename, "r") as post_data_file:
                 data = post_data_file.read()
-
-    if args.header is None:
-        args.header = default_headers
 
     if args.security_token is not None:
         args.session_token = args.security_token
